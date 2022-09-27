9b5dfe7aa085ea8e6183e0de78b4eba9f82661c7
donuts-and <89621960+donuts-and@users.noreply.github.com>
2022-09-27 11:34:22 -0500

    Change preonic keymap

--- keyboards/preonic/keymaps/donuts_and_/keymap.c
+++ keyboards/preonic/keymaps/donuts_and_/keymap.c
@@ -15,8 +15,6 @@
  */
 
 #include QMK_KEYBOARD_H
-#include "muse.h"
-#include "g/keymap_combo.h"
 
 enum preonic_layers {
   _COLEMAK,
@@ -25,174 +23,111 @@ enum preonic_layers {
   _ADJUST
 };
 
-enum preonic_keycodes {
-  LOWER = SAFE_RANGE,
-  RAISE,
-  BACKLIT
+enum keycodes {
+    // Custom oneshot mod implementation with no timers.
+    OS_SHFT = SAFE_RANGE,
+    OS_CTRL,
+    OS_ALT,
+    OS_CMD
 };
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+#define KC_RTAB LSFT(KC_TAB)
+
+#include "g/keymap_combo.h"
+#include "users/callum/oneshot.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_COLEMAK] = LAYOUT_preonic_grid(
-  KC_Q,        KC_W,        KC_F,        KC_P,        KC_B,    _______, _______, KC_J,    KC_L,        KC_U,           KC_Y,          KC_SLSH,
-  ALT_T(KC_A), GUI_T(KC_R), SFT_T(KC_S), CTL_T(KC_T), KC_G,    _______, _______, KC_M,    CTL_T(KC_N), SFT_T(KC_E),    GUI_T(KC_I),   ALT_T(KC_O),
-  KC_Z,        KC_X,        KC_C,        KC_D,        KC_V,    _______, _______, KC_K,    KC_H,        KC_COMM,        KC_DOT,        KC_QUOT,
-  _______,     _______,     _______,     _______,     _______, _______, _______, _______, _______,     _______,        _______,       _______,
-  _______,     _______,     _______,     LOWER,       KC_LSFT, _______, _______, KC_SPC,  RAISE,       _______,        _______,       _______
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    _______, _______, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    _______, _______, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+  _______, _______, _______, LOWER,   KC_LSFT, KC_TAB,  KC_QUOT, KC_SPC,  RAISE,   _______, _______, _______
 ),
 
 [_LOWER] = LAYOUT_preonic_grid(
-  KC_ESC,  KC_ENT,  _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,
-  KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, _______, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, KC_TAB,  _______, _______, _______, _______
+  KC_ESC,  _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,
+  KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, _______, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
+  OS_ALT,  OS_CMD,  OS_SHFT, OS_CTRL, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_ENT,  _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT_preonic_grid(
-  S(KC_1), S(KC_2), S(KC_3),  S(KC_4),    S(KC_5),    _______, _______, S(KC_6),   S(KC_7), S(KC_8), KC_MINS, KC_EQL,
-  KC_1,    KC_2,    KC_3,     KC_4,       KC_5,       _______, _______, KC_6,      KC_7,    KC_8,    KC_9,    KC_0,
-  KC_LBRC,KC_RBRC,S(KC_LBRC), S(KC_RBRC), S(KC_COMM), _______, _______, S(KC_DOT), S(KC_9), S(KC_0), _______, _______,
-  _______, _______, _______,  _______,    _______,    _______, _______, _______,   _______, _______, _______, _______,
-  _______, _______, _______,  _______,    _______,    _______, _______, _______,   _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,
+  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+  KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_GRV,  _______, _______, KC_BSLS, KC_LPRN, KC_RPRN, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_ADJUST] = LAYOUT_preonic_grid(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  _______, _______, _______, _______, _______, _______, _______, C(KC_F6),S(KC_F6),KC_F11,  KC_F12,  _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  RGB_SPI, RGB_HUI, RGB_SAI, RGB_VAI, RGB_MOD, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,
+  RGB_SPD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_RMOD,_______, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,
+  OS_ALT,  OS_CMD,  OS_SHFT, OS_CTRL, RGB_TOG, _______, _______, _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-        case LOWER:
-          if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case RAISE:
-          if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case BACKLIT:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-            #ifdef BACKLIGHT_ENABLE
-              backlight_step();
-            #endif
-            #ifdef RGBLIGHT_ENABLE
-              rgblight_step();
-            #endif
-            #ifdef __AVR__
-            writePinLow(E6);
-            #endif
-          } else {
-            unregister_code(KC_RSFT);
-            #ifdef __AVR__
-            writePinHigh(E6);
-            #endif
-          }
-          return false;
-          break;
-      }
-    return true;
-};
 
-bool muse_mode = false;
-uint8_t last_muse_note = 0;
-uint16_t muse_counter = 0;
-uint8_t muse_offset = 70;
-uint16_t muse_tempo = 50;
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-  if (muse_mode) {
-    if (IS_LAYER_ON(_RAISE)) {
-      if (clockwise) {
-        muse_offset++;
-      } else {
-        muse_offset--;
-      }
-    } else {
-      if (clockwise) {
-        muse_tempo+=1;
-      } else {
-        muse_tempo-=1;
-      }
-    }
-  } else {
-    if (clockwise) {
-      register_code(KC_PGDN);
-      unregister_code(KC_PGDN);
-    } else {
-      register_code(KC_PGUP);
-      unregister_code(KC_PGUP);
-    }
-  }
-    return true;
-}
-
-bool dip_switch_update_user(uint8_t index, bool active) {
-    switch (index) {
-        case 0:
-            if (active) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
-            }
-            break;
-        case 1:
-            if (active) {
-                muse_mode = true;
-            } else {
-                muse_mode = false;
-            }
-    }
-    return true;
-}
-
-
-void matrix_scan_user(void) {
-#ifdef AUDIO_ENABLE
-    if (muse_mode) {
-        if (muse_counter == 0) {
-            uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-            if (muse_note != last_muse_note) {
-                stop_note(compute_freq_for_midi_note(last_muse_note));
-                play_note(compute_freq_for_midi_note(muse_note), 0xF);
-                last_muse_note = muse_note;
-            }
-        }
-        muse_counter = (muse_counter + 1) % muse_tempo;
-    } else {
-        if (muse_counter) {
-            stop_all_notes();
-            muse_counter = 0;
-        }
-    }
-#endif
-}
-
-bool music_mask_user(uint16_t keycode) {
-  switch (keycode) {
-    case RAISE:
+bool is_oneshot_cancel_key(uint16_t keycode) {
+    switch (keycode) {
     case LOWER:
-      return false;
+    case RAISE:
+        return true;
     default:
-      return true;
-  }
+        return false;
+    }
+}
+
+bool is_oneshot_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+    case LOWER:
+    case RAISE:
+    case KC_LSFT:
+    case OS_SHFT:
+    case OS_CTRL:
+    case OS_ALT:
+    case OS_CMD:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool sw_win_active = false;
+bool sw_lang_active = false;
+
+oneshot_state os_shft_state = os_up_unqueued;
+oneshot_state os_ctrl_state = os_up_unqueued;
+oneshot_state os_alt_state = os_up_unqueued;
+oneshot_state os_cmd_state = os_up_unqueued;
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    update_oneshot(
+        &os_shft_state, KC_LSFT, OS_SHFT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_ctrl_state, KC_LCTL, OS_CTRL,
+        keycode, record
+    );
+    update_oneshot(
+        &os_alt_state, KC_LALT, OS_ALT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_cmd_state, KC_LCMD, OS_CMD,
+        keycode, record
+    );
+    return true;
 }
--- keyboards/preonic/keymaps/donuts_and_/rules.mk
+++ keyboards/preonic/keymaps/donuts_and_/rules.mk
@@ -1,3 +1,7 @@
 SRC += muse.c
 VPATH  +=  keyboards/gboards/
 COMBO_ENABLE=yes
+
+SRC += users/callum/oneshot.c
+
+RGBLIGHT_ENABLE = no
